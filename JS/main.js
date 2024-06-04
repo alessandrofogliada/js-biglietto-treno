87aff39e4c8ad3fcf89a9e64e3e1f06482be9584
alessandrofogliada <fogliada.a@gmail.com>
2024-06-04 15:59:24 +0200

    il risultato può avere massimo due numeri decimali

--- JS/main.js
+++ JS/main.js
@@ -6,10 +6,10 @@ let kmDaPercorrere = parseInt( prompt("Quanti chilometri vuoi percorrere ?"));
 
 // chiedere l’età del passeggero
 
-let etaPasseggero = prompt(" Quanti anni hai )");
+let etaPasseggero = parseInt(prompt(" Quanti anni hai" ));
 
 console.log("chilometri che deve percorrere il passeggero" , kmDaPercorrere);
-console.log("età del passeggero" , etaPasseggero)
+console.log("età del passeggero" , etaPasseggero);
 
 // calcolare il prezzo totale del viaggio, secondo queste regole:il prezzo del biglietto è definito in base ai km (0.21 € al km)
 
@@ -19,23 +19,24 @@ console.log("Il costo è di:" , risultato , "€");
 
 // va applicato uno sconto del 20% per i minorenni
 
-const scontoBimbo = ((risultato * 20 ) / 100)
+const scontoBimbo = ((risultato * 20 ) / 100);
 
 // va applicato uno sconto del 40% per gli over 65.
 
-const scontoAnziano = ((risultato * 40 ) / 100)
+const scontoAnziano = ((risultato * 40 ) / 100);
 
 
 if (etaPasseggero <= 17) {
-    let prezzoBimbo = risultato - scontoBimbo;
-    console.log("costo biglietto bimbo" , prezzoBimbo ,"€")
+    let prezzoBimbo = (risultato - scontoBimbo).toFixed(2);
+    console.log("costo biglietto bimbo" , prezzoBimbo ,"€");
 } else if (etaPasseggero >= 65 ){
-    let prezzoAnziano = risultato - scontoAnziano;
-    console.log("costo biglietto anziano" , prezzoAnziano ,"€")
+    let prezzoAnziano = (risultato - scontoAnziano).toFixed(2);
+    console.log("costo biglietto anziano" , prezzoAnziano ,"€");
 } else{
-    console.log("costo biglietto adulto" , risultato , "€")
+    console.log("costo biglietto adulto" , (risultato.toFixed(2)) , "€");
 }
 
-document.getElementById('mio_id').innerHTML = "Il costo del biglietto è di :" , risultato ;
+document.getElementById('mio_id').innerHTML = "Il costo del biglietto è di :" + risultato ;
+
 
 // L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
