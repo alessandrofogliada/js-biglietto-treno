72c42e9e1d781d448270d9528b5e524497a5dde5
alessandrofogliada <fogliada.a@gmail.com>
2024-06-04 15:28:15 +0200

    messa in ordine delle operazioni

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,6 @@
 // specifichiamo il costo al chilometro
 const costoTrasporto = (0.21);
-const scontoBimbo = (20)
-const scontoAnziano = (40)
+
 // chiedere all’utente il numero di chilometri che vuole percorrere
 let kmDaPercorrere = parseInt( prompt("Quanti chilometri vuoi percorrere ?"));
 
@@ -20,14 +19,23 @@ console.log("Il costo è di:" , risultato , "€");
 
 // va applicato uno sconto del 20% per i minorenni
 
-if (etaPasseggero <= 17) {
-    let prezzoBimbo = risultato / scontoBimbo;
-    console.log("costo biglietto bimbo" , prezzoBimbo)
-} else if (etaPasseggero >= 65 ){
-    let prezzoAnziano = risultato / scontoAnziano;
-    console.log("costo biglietto anziano" , prezzoAnziano)
-}
+const scontoBimbo = ((risultato * 20 ) / 100)
 
 // va applicato uno sconto del 40% per gli over 65.
+
+const scontoAnziano = ((risultato * 40 ) / 100)
+
+
+if (etaPasseggero <= 17) {
+    let prezzoBimbo = risultato - scontoBimbo;
+    console.log("costo biglietto bimbo" , prezzoBimbo ,"€")
+} else if (etaPasseggero >= 65 ){
+    let prezzoAnziano = risultato - scontoAnziano;
+    console.log("costo biglietto anziano" , prezzoAnziano ,"€")
+} else{
+    console.log("costo biglietto adulto" , risultato , "€")
+}
+
+document.getElementById('mio_id').innerHTML = "Il costo del biglietto è di :" , risultato ;
 
 // L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
